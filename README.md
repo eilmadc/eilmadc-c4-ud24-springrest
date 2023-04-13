2758d02563188b9ec8334da0dc298fa203b411fb
eilmadc@gmail.com <eilmadc@gmail.com>
2023-04-13 10:41:51 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# eilmadc-c4-ud24-springrest
+ C4 - UD24 - Spring Rest
---
+++ src/main/java/com/example/demo/EilmadcC4Ud24SpringrestApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EilmadcC4Ud24SpringrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EilmadcC4Ud24SpringrestApplication.class, args);
+	}
+
+}
